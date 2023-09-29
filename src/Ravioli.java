0409fd2876425dc24f719d5b1da88f5295d4a603
Anx0Fdez <afernandezrodriguez@danielcastelao.org>
2023-09-29 09:57:27 +0200

    commit_ravioli

---
+++ src/Ravioli.java
@@ -0,0 +1,8 @@
+public class Ravioli {
+    public static void main(String[] args) {
+        System.out.printf("Hola te gustan los Raviolis?");
+        System.out.println("ME GUSTAN LOS RAVIOLIS");
+        // Los raviolis es un plato clasico italiano
+
+    }
+}
