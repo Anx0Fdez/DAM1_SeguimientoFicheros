3d77fc5f59b8dc06b32136fcb93dbbe5c1100168
Anx0Fdez <afernandezrodriguez@danielcastelao.org>
2023-09-29 10:06:42 +0200

    commit_ravioli_main

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-        System.out.println("Este es el primer archivo del seguimiento de ficherosMain ");
-        //
+        System.out.println("Este es el primer archivo del seguimiento de ficheros Main ");
+        // Modificando archivo
 
     }
 }
--- src/Ravioli.java
+++ src/Ravioli.java
@@ -1,7 +1,9 @@
 public class Ravioli {
     public static void main(String[] args) {
         System.out.printf("Hola te gustan los Raviolis?");
-        System.out.println("ME GUSTAN LOS RAVIOLIS");
+        System.out.println("ME GUSTAN LOS RAVIOLIS!!");
+        System.out.println("LOS RAVIOLIS ESTAN BUENOS");
+
         // Los raviolis es un plato clasico italiano
 
     }
