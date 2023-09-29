53481c62a70b78a600140ddcb3a50d22e712ed7f
Anx0Fdez <afernandezrodriguez@danielcastelao.org>
2023-09-29 10:02:14 +0200

    commit_ñoqui

---
+++ src/Ñoqui.java
@@ -0,0 +1,8 @@
+public class Ñoqui {
+    public static void main(String[] args) {
+        System.out.printf("Hola te gustan los Ñoquis?");
+        System.out.println("ME GUSTAN LOS ÑOQUIS");
+        // Los raviolis es un plato clasico italiano
+
+    }
+}
