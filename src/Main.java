7047ffebf20d5e6f953591f881ac16a04358be20
Anx0Fdez <afernandezrodriguez@danielcastelao.org>
2023-09-29 09:46:20 +0200

    hello world

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        System.out.println("Este es el primer archivo del seguimiento de ficherosMain ");
+        //
+
+    }
+}
